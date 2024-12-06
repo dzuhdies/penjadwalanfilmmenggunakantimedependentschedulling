c95cdb054871d4db1ea02ea3ec8c4f68eb1021d8
dzuhdies <120154704+dzuhdies@users.noreply.github.com>
2024-12-06 19:18:04 +0700

    save_data.php

--- SO/api/save_data.php
+++ SO/api/save_data.php
@@ -7,7 +7,6 @@ $ticket_sales = $_POST['ticket_sales'];
 $social_interactions = $_POST['social_interactions'];
 $release_date = $_POST['release_date'];
 
-// Sanitasi input untuk mencegah SQL Injection
 $title = pg_escape_string($conn, $title);
 $rating = floatval($rating);
 $ticket_sales = intval($ticket_sales);
@@ -16,7 +15,6 @@ $social_interactions = intval($social_interactions);
 $sql = "INSERT INTO films (title, rating, ticket_sales, social_interactions, release_date)
         VALUES ('$title', $rating, $ticket_sales, $social_interactions, '$release_date')";
 
-// Menggunakan pg_query() untuk eksekusi query di PostgreSQL
 $result = pg_query($conn, $sql);
 
 if ($result) {
